ae6ed57e556c0ba6728e4cdfa8d97be50482396f
tuuguuwastaken <tuugii59704@gmail.com>
2023-08-14 23:22:06 +0800

    Update MenuService.js

--- src/services/main/MenuService.js
+++ src/services/main/MenuService.js
@@ -1,14 +1,14 @@
 import instance from "@/services/interceptor/interceptor";
 // import {env} from '@/environment/environment.js'
 
-const baseURL = '/api/v1/user'
+const URL = '/api/v1/user'
 class MenuService{
     static getMenu(){
-        console.log(`${baseURL}`)
-        return instance.get(`${baseURL}`)
+        console.log(`${URL}`)
+        return instance.get(`${URL}`)
     }
     static addToMenu(name, description, price) {
-        return instance.post(`${baseURL}/add`, {
+        return instance.post(`${URL}/add`, {
             name:name,
             description:description,
             price:price
